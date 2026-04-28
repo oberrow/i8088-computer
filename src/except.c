fdbf88009a45b7f954fe54485eee22f4daebd524
Omar Berrow <omarkberrow@gmail.com>
2026-04-27 21:08:22 -0400

    misc: halt on fatal exception

--- src/except.c
+++ src/except.c
@@ -6,28 +6,23 @@
 
 #include "frame.h"
 
-void except_div0(struct irq_frame* frame)
-{
+void except_div0(struct irq_frame* frame) {
     while (1)
-        ;
+        asm volatile("hlt" :::"memory");
 }
-void except_trap(struct irq_frame* frame)
-{
+void except_trap(struct irq_frame* frame) {
     while (1)
-        ;
+        asm volatile("hlt" :::"memory");
 }
-void except_nmi(struct irq_frame* frame)
-{
+void except_nmi(struct irq_frame* frame) {
     while (1)
-        ;
+        asm volatile("hlt" :::"memory");
 }
-void except_bp(struct irq_frame* frame)
-{
+void except_bp(struct irq_frame* frame) {
     while (1)
-        ;
+        asm volatile("hlt" :::"memory");
 }
-void except_overflow(struct irq_frame* frame)
-{
+void except_overflow(struct irq_frame* frame) {
     while (1)
-        ;
+        asm volatile("hlt" :::"memory");
 }
