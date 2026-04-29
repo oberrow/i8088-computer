e95e46547e17836ebfd53e5d1405011a0ca7ffa3
Omar Berrow <omarkberrow@gmail.com>
2026-04-29 18:57:07 -0400

    misc: write error messages to uart on exception

--- src/except.c
+++ src/except.c
@@ -5,24 +5,30 @@
 */
 
 #include "frame.h"
+#include "uart.h"
 
 void except_div0(struct irq_frame* frame) {
+    uart_write("DIV0 Exception!", 16);
     while (1)
         asm volatile("hlt" :::"memory");
 }
 void except_trap(struct irq_frame* frame) {
+    uart_write("TRAP Exception!", 16);
     while (1)
         asm volatile("hlt" :::"memory");
 }
 void except_nmi(struct irq_frame* frame) {
+    uart_write("NMI Received!", 14);
     while (1)
         asm volatile("hlt" :::"memory");
 }
 void except_bp(struct irq_frame* frame) {
+    uart_write("INT3 Received!", 15);
     while (1)
         asm volatile("hlt" :::"memory");
 }
 void except_overflow(struct irq_frame* frame) {
+    uart_write("OVERFLOW Exception!", 20);
     while (1)
         asm volatile("hlt" :::"memory");
 }
