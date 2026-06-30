a97b2aed3807ad39fb196a3fb02824a51e2a0005
Omar Berrow <omarkberrow@gmail.com>
2026-06-29 22:52:16 -0400

    misc: make flat assembly test occupy the full 256KiB of flash

--- flat_asm/test.asm
+++ flat_asm/test.asm
@@ -8,11 +8,11 @@ _entry:
     jmp $
 .end:
 
-times 0xfff0 - (_entry.end - _entry) db 0xcc
+times 0x3fff0 - (_entry.end - _entry) db 0xcc
 
 ; At address 0xffff0
 _rstvec:
-    jmp 0xf000:_entry
+    jmp 0xc000:_entry
 .end:
 
-times 0x10000 - ($ - $$) db 0xcc
+times 0x40000 - ($ - $$) db 0xcc
